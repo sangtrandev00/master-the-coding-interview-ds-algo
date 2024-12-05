efbe3ea060d9f1d58f1bb993a56509882b504f8a
sangtrandev00 <nhatsang0101@gmail.com>
2024-12-05 12:01:59 +0700

    add explaination about space-complexity

--- 00_bigO/spaceComplexity.js
+++ 00_bigO/spaceComplexity.js
@@ -1,4 +1,5 @@
 //#5 Space complexity O(1)
+// Tại sao là O(1) => Bởi vì chúng ta chỉ gán cho i một giá trị mỗi 1 lần lặp
 function boooo(n) {
     for (let i = 0; i < n; i++) {
       console.log("booooo");
@@ -6,6 +7,8 @@ function boooo(n) {
   }
   
   // #6 Space complexity O(n)
+  // Chúng ta gán cho một mảng một giá trị môi lần lặp O(n) + 1 => O(n)
+  // Mảng cuối cùng sẽ chứa các danh sách chuỗi "hi" ở các vị trí trong bộ nhớ của mảng chiếm trong máy tính!
   function arrayOfHiNTimes(n) {
     var hiArray = [];
     for (let i = 0; i < n; i++) {
